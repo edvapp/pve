4833a7535139544326b87d83cefbaa2247331aad
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2018-10-24 11:31:53 +0200

    lxc.aa_profile: ...

--- addNodeToCluster.sh
+++ addNodeToCluster.sh
@@ -5,6 +5,8 @@ https://pve.proxmox.com/wiki/Cluster_Manager
 "
 
 echo "ADD NODE TO CLUSTER: pve-app"
+echo "Login via ssh to the node you want to add and:"
+echo "pvecm add IP-ADDRESS-CLUSTER <- IP of first Node in Custer"
 pvecm add 10.0.0.71
 
 echo "SHOW STATUS of: pve-app"
